66c1f4aa0d616c932e9b00a6b267f8372b3697c0
Canyildiz1386 <can.yildiz.1386@gmail.com>
2024-07-23 17:47:12 +0300

    another commit

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 import json
 import time
-import random
 import re
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options
 import logging
+import threading
 
 # Disable all Selenium logging
 logging.getLogger('selenium').setLevel(logging.ERROR)
+
 
 def read_data(file_path):
     try:
@@ -24,6 +25,7 @@ def read_data(file_path):
         print(f'Error reading data from file: {e}')
         return None
 
+
 def read_post_url(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -32,6 +34,7 @@ def read_post_url(file_path):
     except Exception as e:
         print(f'Error reading post URL from file: {e}')
         return None
+
 
 def read_comments(file_path):
     try:
@@ -42,9 +45,10 @@ def read_comments(file_path):
         print(f'Error reading comments from file: {e}')
         return []
 
+
 def filter_bmp_chars(text):
-    # Filter out characters not in BMP (Basic Multilingual Plane)
     return re.sub(r'[^\u0000-\uFFFF]', '', text)
+
 
 def login(driver, username, password):
     try:
@@ -68,6 +72,7 @@ def login(driver, username, password):
         print(f'Failed to login with username {username}: {e}')
         return False
 
+
 def post_comment(driver, post_url, comment_text):
     try:
         driver.get(post_url)
@@ -77,7 +82,7 @@ def post_comment(driver, post_url, comment_text):
 
         comment_box = driver.find_element(By.CSS_SELECTOR, "textarea")
         comment_box.click()
-        comment_box = WebDriverWait(driver, 10).until(
+        comment_box = WebDriverWait(driver, 5).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "textarea"))
         )
 
@@ -88,6 +93,26 @@ def post_comment(driver, post_url, comment_text):
     except Exception as e:
         print(f'Failed to post comment: {e}')
         return False
+
+
+def post_comment_for_account(account, post_url, comment):
+    username = account['username']
+    password = account['password']
+    comment = filter_bmp_chars(comment)
+
+    firefox_options = Options()
+    firefox_options.add_argument("--disable-gpu")
+    firefox_options.add_argument("--log-level=3")
+    firefox_options.add_argument("--headless")
+
+    driver = webdriver.Firefox(service=Service('geckodriver.exe'), options=firefox_options)
+
+    if login(driver, username, password):
+        success = post_comment(driver, post_url, comment)
+        if success:
+            print(f'{username} posted comment: "{comment}" on {post_url}')
+    driver.quit()
+
 
 def post_comments(data, post_url, comments):
     if data is None:
@@ -101,39 +126,32 @@ def post_comments(data, post_url, comments):
         print('Not enough comments for the number of users')
         return
 
+    threads = []
+
     for i, account in enumerate(data['accounts']):
         if i >= num_comments:
             print('Not enough comments for all users')
             break
 
-        username = account['username']
-        password = account['password']
-        comment = filter_bmp_chars(comments[i])
-        
-        chrome_options = Options()
-        chrome_options.add_argument("--disable-gpu")
-        chrome_options.add_argument("--log-level=3") 
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
-        
-        if login(driver, username, password):
-            success = False
-            while not success:
-                if post_comment(driver, post_url, comment):
-                    success = True
-                    print(f'{username} posted comment: "{comment}" on {post_url}')
-                else:
-                    time.sleep(1)
+        thread = threading.Thread(target=post_comment_for_account, args=(account, post_url, comments[i]))
+        threads.append(thread)
+        thread.start()
 
-        driver.quit()
+    for thread in threads:
+        thread.join()
     print("Finished")
+
 
 if __name__ == "__main__":
     data_file = 'accounts.json'
     post_url_file = 'post_url.txt'
     comments_file = 'comments.txt'
 
+    start = time.time()
     data = read_data(data_file)
     post_url = read_post_url(post_url_file)
     comments = read_comments(comments_file)
 
     post_comments(data, post_url, comments)
+    finish = time.time() - start
+    print(f'Finished in {round(finish, 2)} seconds')
